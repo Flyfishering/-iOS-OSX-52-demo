48707b48132afe996700f0c9b463d26bb07a0178
binbin <2426715336@qq.com>
2016-09-01 14:59:59 +0800

    初始化工程

---
+++ -iOS-OSX-52-demo/-iOS-OSX-52-demoTests/_iOS_OSX_52_demoTests.m
@@ -0,0 +1,39 @@
+//
+//  _iOS_OSX_52_demoTests.m
+//  -iOS-OSX-52-demoTests
+//
+//  Created by mac on 16/9/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _iOS_OSX_52_demoTests : XCTestCase
+
+@end
+
+@implementation _iOS_OSX_52_demoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
